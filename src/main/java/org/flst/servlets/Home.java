515d898aa7138684fac36575715c6d997f15d794
GuillaumeVDH <Guillaume.Vanderhaegen@gmail.com>
2016-01-05 10:54:31 +0100

    Starting servlet work

---
+++ src/main/java/org/flst/servlets/Home.java
@@ -0,0 +1,27 @@
+package org.flst.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by guillaumevdh on 04/01/16.
+ */
+public class Home extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1> HELLO FROM HttpServlet </h1>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
